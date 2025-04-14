755c34ae28b44bcf9de44f00826daedbcb677131
MARTIN MA <124866191+MartinYu0510@users.noreply.github.com>
2025-04-15 00:02:54 +0800

    Delete Epic 5/specialist directory

--- Epic 5/specialist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add/', views.add_accommodations,name='add_accommodations'),
-     path('api_add/', views.api_add,name='api_add'),
-     path('api_edit/', views.api_edit,name='api_edit'),
-]
