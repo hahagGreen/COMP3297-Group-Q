8e750701c77510cfd6e990e4f3e21d4ce61835f2
MARTIN MA <124866191+MartinYu0510@users.noreply.github.com>
2025-04-14 21:45:07 +0800

    Update views.py
    Add serializer

--- Epic3/accommodations/views.py
+++ Epic3/accommodations/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.http import JsonResponse
-from .models import Accommodation,Rating
+from .models import Accommodation,Rating, User
+from .serializers import AccommodationSerializer
 # Create your views here.
 
 def view_accommodations(request):
@@ -13,32 +16,24 @@ def view_accommodations(request):
         return render(request, 'view.html', {'accommodation': accommodation, 'rate': rate})
     return render(request, 'demo.html', {'accommodations': accommodations})
 
+# api for fetching accommodation details
+@api_view(['GET','POST'])
 def api_view(request):
-    print(request.GET.get('id'))
-    if request.method == 'GET':
-        accommodation_id = request.GET.get('id')
-        if not accommodation_id:
-            return JsonResponse({'error': 'Accommodation ID is required'}, status=400)
+    userId = request.POST.get('userId')
+    try:
+        user = User.objects.get(user_id=userId)
+        print(user.name)
+    except User.DoesNotExist:
+        return JsonResponse({'error': 'User not found'}, status=404)
+    accommodation_id = request.GET.get('accId')
+    if not accommodation_id:
+        return JsonResponse({'error': 'Accommodation ID is required'}, status=400)
         
-        try:
-            accommodation = Accommodation.objects.get(accommodation_id=accommodation_id)
-            accommodation_details = {
-                'id': accommodation_id,
-                'startDate': accommodation.availability_start,
-                'endDate': accommodation.availability_end,
-                'type': accommodation.type,
-                'numOfBeds': accommodation.beds,
-                'numOfBedrooms': accommodation.bedrooms,
-                'price': accommodation.price,
-                'address': accommodation.address,
-                'geo_address': accommodation.geo_address,
-                'latitude': accommodation.latitude,
-                'longitude': accommodation.longitude,
-                'is_reserved': "yes" if accommodation.is_reserved else "no",
-            }
-            return JsonResponse(accommodation_details)
-        except Accommodation.DoesNotExist:
-            return JsonResponse({'error': 'Accommodation not found'}, status=404)
-    else:
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
+    try:
+        accommodation = get_object_or_404(Accommodation,pk=accommodation_id)
+        serializers = AccommodationSerializer(accommodation)
+        return Response(serializers.data)
+    except Accommodation.DoesNotExist:
+        return JsonResponse({'error': 'Accommodation not found'}, status=404)
+
 
