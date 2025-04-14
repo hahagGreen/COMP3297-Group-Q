f3141d54a8a282bccb8bd5971b4e2a52bfdbd794
MARTIN MA <124866191+MartinYu0510@users.noreply.github.com>
2025-04-15 00:02:40 +0800

    Update urls.py

--- Epic 5/unihaven/urls.py
+++ Epic 5/unihaven/urls.py
@@ -21,5 +21,4 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accommodations/', include('accommodations.urls')),
-    path('specialist/', include('specialist.urls')),
 ]
