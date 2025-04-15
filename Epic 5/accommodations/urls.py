545d172eedc90e1a2a4a3a6e096b31eecbb0249b
MARTIN MA <124866191+MartinYu0510@users.noreply.github.com>
2025-04-15 23:27:40 +0800

    Update urls.py

--- Epic 5/accommodations/urls.py
+++ Epic 5/accommodations/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view/', views.view_accommodations,name='view_accommodations'),
     path('api_rate', views.api_rate,name='api_rate'),
 ]
