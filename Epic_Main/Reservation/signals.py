fe60b9d3265182bc681c8467d79af3e1b93b9ff6
hahagGreen <147497128+hahagGreen@users.noreply.github.com>
2025-04-16 19:45:35 +0800

    Add files via upload

--- Epic_Main/Reservation/signals.py
+++ Epic_Main/Reservation/signals.py
@@ -2,14 +2,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
-from .models import Reservation, User
+from .models import Reservation, Student
 
 
 @receiver(post_save, sender=Reservation)
 def send_reservation_notification(sender, instance, **kwargs):
     """Send email notifications when a reservation is confirmed or canceled."""
     if instance.status in [Reservation.CONFIRMED, Reservation.CANCELED]:
-        specialists = User.objects.filter(role=User.SPECIALIST)
+        specialists = Student.objects.filter(role=Student.SPECIALIST)
         emails = [s.email for s in specialists]
         
         subject = f"Reservation {instance.reservation_id} {instance.get_status_display()}"
