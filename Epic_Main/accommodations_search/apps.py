b19171adfc88cb46c26f500bce1bc2f4010a6c8f
yfQ04 <146961757+yfQ04@users.noreply.github.com>
2025-04-26 17:59:42 +0800

    update new epic main

--- Epic_Main/accommodations_search/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccommodationsSearchConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "accommodations_search"
--- Epic_Main/accommodations_search/urls.py
+++
@@ -1,8 +0,0 @@
-# accommodations/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('search/', views.search_accommodations, name='search_accommodations'),
-    path('api/search/', views.Search_Accommodations_API.as_view(), name='search_accommodations_api'),
-]
