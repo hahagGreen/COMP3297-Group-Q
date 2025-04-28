8355cba87059dbcad90c7d6f48c3292957401e58
MARTIN MA <124866191+MartinYu0510@users.noreply.github.com>
2025-04-29 03:29:20 +0800

    Add files via upload

--- Epic_Main/accommodations/urls.py
+++ Epic_Main/accommodations/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('view/', views.view_accommodations, name='view_accommodations'),
-    path('api_view', views.api_viewDetails, name='api_view'),
-    path('api_search', views.api_search, name='api_search'),
+    path('api_view', views.ApiViewDetails.as_view(), name='api_view'),
+    path('api_search', views.ApiSearchView.as_view(), name='api_search'),
     path('api_rate', views.ApiRateView.as_view(), name='api_rate'),
 ]
 
